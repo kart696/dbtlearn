daa00c882ba4d4081ba5a18b4ab7c321509ab0bd
kaady <kart696@gmail.com>
2023-04-02 01:14:29 +0100

    modernization

--- models/src/src_listings.sql
+++ models/src/src_listings.sql
@@ -4,12 +4,12 @@ with raw_listings as (
 select 
     id as listing_id,
     name as listing_name,
-    --listing_url,
+    listing_url,
     room_type,
     minimum_nights,
     host_id,
-    price as price_str
-    --created_at,
-    --updated_at
+    price as price_str,
+    created_at,
+    updated_at
 from 
     raw_listings
